9def2f3d8d9867016851b849c68539796c0a5c24
Zmitser <donnausamin@gmail.com>
2022-09-14 08:27:22 +0300

    финальный файл

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-const name = 'Robb';
-const upperName = name.toUpperCase();
+const filterString = (str, char) => {
+  let i = 0;
+  let result = '';
+  while (i < str.length) {
+    const currentChar = str[i];
+    if (currentChar !== char) {
+      result = `${result}${currentChar}`;
+    }
+    i += 1;
+  }
 
-alert( upperName); 
+  return result;
+};
+  const jjj = "Ебаные пидорас";
+  const kkk = "Е";
+  console.log(filterString(jjj, kkk));
